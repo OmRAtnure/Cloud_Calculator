58ff187155fb649208f148e9c16c9b9ec26f9bf5
OmRAtnure <atnureom@gmail.com>
2026-04-07 14:27:10 +0530

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void Calculation(int a,int b,char op) {
+  switch(op)
+    case +:
+      cout<<"Addition of "<<a<<" and "<<b<<" is: "<<a+b;
+      break;
+    case -:
+      cout<<"Substraction of "<<a<<" and "<<b<<" is: "<<a-b;
+      break;
+    case *:
+      cout<<"Multiplication of "<<a<<" and "<<b<<" is: "<<a*b;
+      break;
+    case /:
+      cout<<"Division of "<<a<<" and "<<b<<" is: "<<a/b;
+      break;
+    default :
+        cout<<"wrong operation"<<endl;
+}
+
+int main()  {
+  int a,b;
+  char operation;
+  // cout<<"-------Menu-------";
+  cout<<"Choose Operation (+,-,*,/)";
+  cin>>operation;
+  cout<<"Enter First Number: ";
+  cin>>a;
+  cout<<"Enter Second Number: ";
+  cin>>b;
+  Calculation(a,b,operation);
+  return 0;
+  
+}
